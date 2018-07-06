168e1a7401d0ecb929e58230477a97273ead8c13
Cobey Potter <cobey.potter@wellfireinteractive.com>
2018-07-05 22:54:48 -0400

    Add ability to take aliases from a supplied config

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const path = require('path')
 
 module.exports = (bundler) => {
-    let options;
+    let aliases = bundler.options.aliases || false
 
-    try {
-
-        options = require(path.resolve(process.cwd(), '.aliasrc.js'));
-
-    } catch(e) {
-        // no handler
+    if (!aliases) {
+        try {
+            aliases = require(path.join(bundler.options.rootDir, '/.aliasrc.js'))
+        } catch(e) {
+            // no handler
+            console.log(`Can't find .aliasrc.js: ${ e }`)
+        }
     }
 
-    if (options) {
-        let originResolver = bundler.resolver;
-        let originResolve = originResolver.resolve;
+    if (aliases) {
+        let originResolver = bundler.resolver
+        let originResolve = originResolver.resolve
 
         originResolver.resolve = (filename, parent) => {
             if (filename) {
-                for (let name in options) {
-                    let alias = options[name];
+                for (let name in aliases) {
+                    let alias = path.join(bundler.options.rootDir, aliases[name])
 
                     if (filename === name || startsWith(filename, name + '/')) {
                         if (filename !== alias && !startsWith(filename, alias + '/')) {
@@ -27,36 +27,36 @@ module.exports = (bundler) => {
                              * compatible with new version(1.7.0)
                              * @type {boolean}
                              */
-                            let newVersion = !!originResolver.packageCache;
+                            let newVersion = !!originResolver.packageCache
 
                             if (newVersion) {
-                                filename = alias.replace(bundler.options.rootDir, '') + filename.substr(name.length);
+                                filename = alias.replace(bundler.options.rootDir, '') + filename.substr(name.length)
                             } else {
-                                filename = alias + filename.substr(name.length);
+                                filename = alias + filename.substr(name.length)
                             }
                         }
                     }
                 }
             }
 
-            return originResolve.call(originResolver, filename, parent);
+            return originResolve.call(originResolver, filename, parent)
         }
     }
 }
 
-function startsWith(string, searchString) {
-    const stringLength = string.length;
-    const searchLength = searchString.length;
+function startsWith (string, searchString) {
+    const stringLength = string.length
+    const searchLength = searchString.length
 
     // early out if the search length is greater than the search string
     if(searchLength > stringLength) {
-        return false;
+        return false
     }
-    let index = -1;
+    let index = -1
     while(++index < searchLength) {
         if(string.charCodeAt(index) !== searchString.charCodeAt(index)) {
-            return false;
+            return false
         }
     }
-    return true;
+    return true
 }
